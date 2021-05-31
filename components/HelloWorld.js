7fca19e17ad0442c3436e527cc2cf423f961f42d
Jesus Fernandez <fernandezjodar.jesus@gmail.com>
2021-05-31 17:23:25 +0200

    Add content

--- components/HelloWorld.js
+++ components/HelloWorld.js
@@ -1,12 +1,13 @@
 Vue.component('hello-world', {
     template: `<header>
                     <h1>{{ title }}</h1>
-                    <h3>{{ message }}</h3>
+                    <hr />
+                    <p>{{ message }}</p>
                </header>`,
     data() {
         return {
-            title: 'Hello World!',
-            message: 'Bienvenido al curso de Vuejs'
+            title: 'Bienvenido al curso de Vuejs',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         }
     }
 })
